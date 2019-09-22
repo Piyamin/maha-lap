047236b4db86381f970745faf2f8543a909977d7
Piyamin <piyaminjaima42@gmail.com>
2019-09-22 10:12:36 +0700

    Add tamnaiWitthDay Add config

---
+++ maha-lap.js
@@ -0,0 +1,11 @@
+class MahaLap {
+    static tamnaiWithDay(day) {
+        if (day === "อังคาร") {
+            return 3;
+        } else {
+            return 2;
+        }
+
+    }
+}
+module.exports = MahaLap;
---
+++ maha-lap.test.js
@@ -0,0 +1,11 @@
+const Mahalap = require("./maha-lap");
+describe("Test Maha-Lap", () => {
+    test("Test Tamnai with day", () => {
+        const result = Mahalap.tamnaiWithDay("จันทร์");
+        expect(result).toEqual(2);
+    });
+    test("Test Tamnai with day", () => {
+        const result = Mahalap.tamnaiWithDay("อังคาร");
+        expect(result).toEqual(3);
+    });
+});
